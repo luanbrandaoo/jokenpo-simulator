425894db0b42b08a60e713fcac26f2dc3ead0cf2
Luan <luanbrandaodeoliveira@gmail.com>
2024-02-18 20:30:02 -0300

    first PC layout

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ quantityScissors.addEventListener('change', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    disableResponsiveness();
     if (!navigator.userAgent.includes("Chrome") && !navigator.userAgent.includes("Chromium")) {
         alert("This page has not been fully tested on non-Chromium-based browsers. Some bugs (mainly in the UI) may appear. We recommend using Chrome for a better experience.");
     }
